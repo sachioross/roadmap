e2b97255de5b7d45e282b055aff56ce5716b9fe6
Douglas Ross <dross@xumak.com>
2015-01-02 13:06:40 -0600

    Adding in ability to submit a feature request in the angular controllers

--- scripts/roadmap.js
+++ scripts/roadmap.js
@@ -9,10 +9,16 @@ var trello = {
     key: "key=cb23c0648098dd2dcdd2964359d4b5ba",
     token: "token=3a7193245ac9022c730f3c828b1ba942bd4e28bae32b11b5a72d243d84097f47", 
     cardsAllParam: "cards=all",
-    listAllParam: "lists=all"
+    listAllParam: "lists=all",
+    writeToken: "token=6491b479ab617f1a5b38307202cd6f5151e563570bca24dd284d3d9d2289a4bd",
+    requestedListId: "54a360f7f59a9f0bedce16f9",
+    keyValue: "cb23c0648098dd2dcdd2964359d4b5ba",
+    tokenValue: "3a7193245ac9022c730f3c828b1ba942bd4e28bae32b11b5a72d243d84097f47",
+    writeTokenValue: "6491b479ab617f1a5b38307202cd6f5151e563570bca24dd284d3d9d2289a4bd"
 }
 
 trello.keyAndToken = trello.key + trello.queryParamConcat + trello.token;
+trello.keyAndWriteToken = trello.key + trello.queryParamConcat + trello.writeToken;
 
 function concat() {
     var concatenated = "";
@@ -28,7 +34,7 @@ function createPromiseSuccessForIndex($scope, promise, index) {
     })
 }
 
-app.controller("roadmapBoard", ['$http', '$scope', function($http, $scope) {
+app.controller("RoadmapBoard", ['$http', '$scope', function($http, $scope) {
     var trelloBoardAddress = concat(trello.boardsUrl,
                       trello.bedrockBoardId,
                       trello.queryParamMark,
@@ -40,10 +46,16 @@ app.controller("roadmapBoard", ['$http', '$scope', function($http, $scope) {
     
     $http.get(trelloBoardAddress)
         .success(function(data, status){
+        
+            $scope.lists = new Array();
+        
+            for(i in data.lists) {    
+                if (data.lists[i].name !== "Requested") {
+                    $scope.lists.push(data.lists[i]);
+                }
+            }
             
-            $scope.lists = data.lists;
-            
-            for (var i in $scope.lists) {    
+            for (var i in $scope.lists) {
                 var promise = $http.get(concat(trello.listsUrl,
                        $scope.lists[i].id,
                        "/cards",
@@ -53,10 +65,63 @@ app.controller("roadmapBoard", ['$http', '$scope', function($http, $scope) {
                     console.log("error: " + status);
                 });
                 createPromiseSuccessForIndex($scope, promise, i);
+                
             }
         })
         .error(function(data, status){
             console.log("error: " + status);
     });
     
+}])
+.controller("RegisterController", ['$scope', '$http', function($scope, $http) {
+    
+    $scope.register = function() {
+
+        
+        var trelloWriteListAddress = concat(trello.listsUrl,
+                                        trello.requestedListId,
+                                        "/cards", 
+                                        trello.queryParamMark,
+                                        trello.keyAndWriteToken, 
+                                        trello.queryParamConcat, 
+                                        "name=",
+                                        $scope.feature.title,
+                                        trello.queryParamConcat,
+                                        "description=",
+                                        $scope.feature.description,
+                                        trello.queryParamConcat,
+                                        "due=null");
+        
+        var trelloWriteData = {
+            key: trello.keyValue,
+            token: trello.writeTokenValue,
+            name: $scope.feature.title,
+            description: $scope.feature.description,
+            due: "null"
+        }
+        
+        var req = {
+         method: 'POST',
+         url: trelloWriteListAddress,
+         headers: {
+           'Content-Type': undefined
+         },
+         data: trelloWriteData
+        }
+
+        
+        
+        
+        $http(req)
+            .success(function(data, status) {
+                console.log("Success");
+                console.log(data);
+                $scope.feature.status = "success";
+        })
+            .error(function(data, status) {
+                console.log("error");
+                console.log(status);
+                $scope.feature.status = "failure";
+        });
+    };
 }]);
